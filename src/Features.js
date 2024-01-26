f93c45ebc59ec7425dffcfab8896322894966fec
Nwe Nwe Lin <nwenwelinn@gicjp.com>
2024-01-26 09:45:12 +0630

    update

---
+++ src/Features.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Featuretwo from './Featuretwo';
+import Featureone from './Featureone';
+import Featurethree from './Featurethree';
+
+export default function Features() {
+  return (
+    <div id="features">
+      <Featureone></Featureone>
+      <Featuretwo></Featuretwo>
+      <Featurethree></Featurethree>
+    </div>
+  )
+}
